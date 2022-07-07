dc39d72a45267777ecb2f7c271961f78597b0127
ASkovorodker <viktorkiselyev@gmail.com>
2022-07-07 12:00:54 +0500

    body

---
+++ MoneyBox.py
@@ -0,0 +1,6 @@
+class MoneyBox:
+    def __init__(self, capacity): # конструктор с аргументом – вместимость копилки
+
+    def can_add(self, v): # True, если можно добавить v монет, False иначе
+
+    def add(self, v): # положить v монет в копилку. Гарантируется, что метод add(self, v) будет вызываться только если can_add(self, v) – True.
