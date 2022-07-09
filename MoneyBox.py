b5a2eaa9dff454bb69f54c6cea110625673ce447
ASkovorodker <viktorkiselyev@gmail.com>
2022-07-09 21:44:15 +0500

    MoneyBox_v2

--- MoneyBox.py
+++ MoneyBox.py
@@ -2,16 +2,20 @@ class MoneyBox:
     def __init__(self, capacity=0): # конструктор с аргументом – вместимость копилки
         self.capacity = capacity
         self.sum = 0
+        self.status = True
 
     def can_add(self, v): # True, если можно добавить v монет, False иначе
         if self.capacity > v:
-            return True
+            self.status = True
+            #return True
         else:
-            return False
+            self.status = False
+            #return False
 
     def add(self, v): # положить v монет в копилку. Гарантируется, что метод add(self, v) будет вызываться только если can_add(self, v) – True.
         #self.sum += v
-        if self.can_add(v) == True:
+        self.can_add(v)
+        if self.status is True:
             self.sum += v
 
 a = MoneyBox(5)
