f9b0594514c8aea57e8ad7f7f28507b146c21ceb
ASkovorodker <viktorkiselyev@gmail.com>
2022-07-14 18:54:46 +0500

    finally

--- MoneyBox.py
+++ MoneyBox.py
@@ -1,35 +1,18 @@
 class MoneyBox:
-    def __init__(self, capacity=0, saving=0): # конструктор с аргументом – вместимость копилки
+    def __init__(self, capacity):
         self.capacity = capacity
-        self.saving = saving
-        #self.free = 0
-        #self.status = True
 
-    def can_add(self, v): # True, если можно добавить v монет, False иначе
-        # проверяем в can_add можно ли положить монету в копилку (если мы положим монету в копилку, не привысит ли она capacity?)
-        if self.capacity > v:
-            #self.status = True
+    def can_add(self, v):
+        if self.capacity >= v:
             return True
         else:
-            #self.status = False
             return False
 
-    def add(self, v): # положить v монет в копилку. Гарантируется, что метод add(self, v) будет вызываться только если can_add(self, v) – True.
-        if self.can_add(v) is True: # проверяем в add, что вернула can_add. если все хорошо, добавляем монеты в копилку и возвращаем True или False
-        #if self.status is True:
-            self.saving += v
-            self.capacity = self.capacity - v
-            return self.capacity
-        else:
-            #self.free = self.capacity - self.saving
-            return self.capacity
+    def add(self, v):
+        self.capacity = self.capacity - v
+
 
 a = MoneyBox(15)
 a.add(5)
 a.add(9)
 a.add(3)
-
-
-#Не забывайте, что результат нужно вывести исключительно через return.
-# Никаких print'ов. Вывести нужно can_add: True/False и
-# Внимание! add: доступное число монет, которое можно внести (Напр.: Capacity = 10. Внесли 5. Осталось 5. Это наш ответ).
